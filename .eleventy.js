cade63363e31ff082ff26aca12202dd8264bf4da
Nicolas Hoizey <nicolas@hoizey.com>
2020-10-20 18:03:59 +0200

    Fix issues with loading the library

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const link_to = require('lib/link_to.js');
+const link_to = require('./lib/link_to.js');
 
-module.exports = {
-  configFunction: (eleventyConfig) => {
-    eleventyConfig.addNunjucksTag('link_to', link_to);
-  },
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addNunjucksTag('link_to', link_to);
 };
